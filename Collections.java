8bae05a7f9de63c3a383daf8bd41a216a50a01e6
Bainbridge-Philip <pobainbridge@gmail.com>
2017-01-25 17:50:23 -0600

    Add files via upload

--- Collections.java
+++ Collections.java
@@ -26,10 +26,12 @@ public class Collections {
 
     public static void makeSalad() {
 
-        System.out.println("ArrayList");
+        System.out.println("\nList Example - ArrayList");
         printLineSeperator();
 
         ArrayList<String> salad = new ArrayList();
+
+        // populate our list
         salad.add("Lettuce");
         salad.add("Bacon");
         salad.add("spinach");
@@ -60,9 +62,11 @@ public class Collections {
 
         System.out.println("Our salad consists of: " + salad);
 
+        // do we have these ingredients?
         System.out.println("Does our salad have Tomatoes? " + ((salad.contains("Tomatoes")) ? "Yes" : "No"));
         System.out.println("Does our salad have Peas? " + ((salad.contains("Peas")) ? "Yes" : "No"));
 
+        // clear the collection
         salad.clear();
 
         System.out.println("Our salad consists of: " + salad );
@@ -71,11 +75,12 @@ public class Collections {
 
     public static void searchScriptures() {
 
-        System.out.println("HashMap");
+        System.out.println("\nMap Example - HashMap");
         printLineSeperator();
 
         HashMap<String, String> books = new HashMap();
 
+        // populate our map
         books.put("Nephi", "The Book of Mormon");
         books.put("Daniel", "The Old Testament");
         books.put("Mathew", "The New Testament");
@@ -84,16 +89,20 @@ public class Collections {
 
         System.out.println("Books of Scriptures " + books);
 
+        // find out which book Moses is in
         System.out.println("The book of Moses is in " + books.get("Moses"));
 
+        // Moses needs to be fixed. It's is in The Pearl of Great Price
         books.put("Moses", "The Pearl of Great Price");
 
         System.out.println("The book of Moses is in " + books.get("Moses"));
 
+        // Baruch isn't part of the KJV bible
         books.remove("Baruch");
 
         System.out.println("Books of Scriptures " + books);
 
+        // do we have certain books?
         System.out.println("Do we have a book from The Old Testament? " + ((books.containsValue("The Old Testament")) ? "Yes" : "No"));
         System.out.println("Do we have a book from The Doctrine and Covenants? " + ((books.containsValue("The Doctrine and Covenants")) ? "Yes" : "No"));
 
@@ -103,8 +112,12 @@ public class Collections {
 
     public static void collectFruits() {
 
+        System.out.println("\nSet Example - HashSet");
+        printLineSeperator();
+
         HashSet<String> fruits = new HashSet();
 
+        // populate our set
         fruits.add("Apple");
         fruits.add("Pear");
         fruits.add("Banana");
@@ -113,15 +126,18 @@ public class Collections {
 
         System.out.println("The Fruit Bowl has " + fruits.size() + " fruits: " + fruits);
 
+        // HashSets can't have duplicates, but we'll try anyway
         fruits.add("Lime");
         fruits.add("Pear");
 
         System.out.println("The Fruit Bowl has " + fruits.size() + " fruits: " + fruits);
 
+        // don't need these in our bowl
         fruits.remove("Lime");
 
         System.out.println("The fruit bowl has " + fruits.size() + " fruits: " + fruits);
 
+        // do we have these fruits?
         System.out.println("Does our fruit bowl have Apples? " + ((fruits.contains("Apple")) ? "Yes" : "No"));
         System.out.println("Does our fruie bowl have Limes? " + ((fruits.contains("Lime")) ? "Yes" : "No"));
 
@@ -129,21 +145,27 @@ public class Collections {
 
     private static void crazyCountdown() {
 
+        System.out.println("\nTree Example - TreeSet");
+        printLineSeperator();
+
         TreeSet<Integer> countdown = new TreeSet<Integer>();
 
+        // populate our tree
         countdown.add(1);
         countdown.add(15);
         countdown.add(9);
         countdown.add(20);
         countdown.add(18);
-        countdown.add(15);
+        countdown.add(15);  // duplicates don't work
         countdown.add(5);
         countdown.add(3);
         countdown.add(11);
-        countdown.add(3);
+        countdown.add(3);   // duplicates don't work
 
+        // we don't need 18
         countdown.remove(18);
 
+        // print our coundown (or really our countup)
         for (int num : countdown) {
             System.out.println("T minus " + num + " and counting");
         }
